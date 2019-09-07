aa3be73549ffe54e29cf98e3c6d1fd29f7636348
Stéphane GRILLON <sgrillon@hotmail.fr>
2019-09-08 00:03:19 +0200

    add "hello" in bakery app

--- bakery-front/src/app/public/public-routing.module.ts
+++ bakery-front/src/app/public/public-routing.module.ts
@@ -5,9 +5,15 @@ import { CommonModule } from '@angular/common';
 import { RoleGuardService } from '../shared/guards/role-guard.service';
 import { CONTRIBUTOR_ROLE } from '../shared/constant/app.constants';
 import { AuthGuardService } from '../shared/guards/auth-guard.service';
+import { Popup1Component } from './home/popup1.component';
+import { Popup2Component } from './home/popup2.component';
+import { Popup3Component } from './home/popup3.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'popup1', component: Popup1Component },
+  { path: 'popup2', component: Popup2Component },
+  { path: 'popup3', component: Popup3Component },
   { path: '**', redirectTo: '/error/404' }
 ];
 
--- bakery-front/src/app/public/public.module.ts
+++ bakery-front/src/app/public/public.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { PublicRoutingModule } from './public-routing.module';
+import { Popup1Component } from './home/popup1.component';
+import { Popup2Component } from './home/popup2.component';
+import { Popup3Component } from './home/popup3.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    Popup1Component,
+    Popup2Component,
+    Popup3Component
+  ],
   imports: [
     SharedModule,
     PublicRoutingModule
